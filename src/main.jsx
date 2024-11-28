c6e10f6bbbdbd0ca7890ad7fe1af713bca8ba123
unknown <jaydeep.gedam444@gmail.com>
2024-11-28 14:58:25 +0530

    fixing errors-2

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@ import "./index.css"
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="*" element={<App />} /> {/* Match all paths */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
